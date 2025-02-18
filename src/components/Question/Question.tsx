49ad11979b2f65dc62aca05896b28315e29f1f38
fudgeu <patrickzambonie@gmail.com>
2025-02-18 16:16:33 -0500

    Remove markdown testing

--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -36,20 +36,17 @@ export default function Question({ question, number }: QuestionProps) {
     <div className={styles.questionArea}>
       <div className={styles.questionsContainer}>
         <h2>{`Question ${number}`}</h2>
-        {/*{question.map((question, i) => <p key={i}>{question}</p>)}*/}
-        {/*<p>*/}
-        {/*  Write some code that generates a fibonacci sequence, or something. Something that works at all. It just needs*/}
-        {/*  to return anything. Something.*/}
-        {/*</p>*/}
-        {/*<p>*/}
-        {/*  Anything at all works. It doesn't have to be a fibonacci sequence. It could be complete garbage. Professors*/}
-        {/*  ever don't check the answers, anyway*/}
-        {/*</p>*/}
-        {/*<p>Here, have some example output:</p>*/}
-        {/*<CodeBlock>0, 1, 1, 2, 3, 5, ...</CodeBlock>*/}
-        <Markdown>
-          {'# Hi, *Pluto*!\n- list item 1\n- list item 2\n```py\n# test\n```'}
-        </Markdown>
+        {question.map((question, i) => <p key={i}>{question}</p>)}
+        <p>
+          Write some code that generates a fibonacci sequence, or something. Something that works at all. It just needs
+          to return anything. Something.
+        </p>
+        <p>
+          Anything at all works. It doesn't have to be a fibonacci sequence. It could be complete garbage. Professors
+          ever don't check the answers, anyway
+        </p>
+        <p>Here, have some example output:</p>
+        <CodeBlock>0, 1, 1, 2, 3, 5, ...</CodeBlock>
       </div>
 
       <div className={styles.editorContainer}>
